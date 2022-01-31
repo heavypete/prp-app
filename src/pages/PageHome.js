06b9dffe722dd839ee049a48a9f05e1cccd0a2e3
heavypete <70510263+heavypete@users.noreply.github.com>
2022-01-31 18:01:21 +0100

    add audiomgr, piano, Rmgr, home, midkeybrd, keymap

--- src/pages/PageHome.js
+++ src/pages/PageHome.js
@@ -2,9 +2,18 @@ import React from "react";
 
 const PageHome = () => {
   const { siteStatus, toggleSiteStatus } = useContext(AppContext);
+
   return (
-    <fieldset>
-      <legend className="legend">Login</legend>
-    </fieldset>
+    <div>
+      This is the welcome page :D
+      <p>
+        Current status is <span className="highlight">{siteStatus}</span>
+      </p>
+      <p>
+        <button onClick={toggleStatus}> Toggle Status</button>
+      </p>
+    </div>
   );
 };
+
+export default PageHome;
